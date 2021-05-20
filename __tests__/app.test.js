f2cd99c01d98fb6c0a4f9fa55b13d37a57e14a5a
Chase Abbott <cabbott93@gmail.com>
2021-05-19 17:29:41 -0700

    add delete path

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -28,11 +28,9 @@ describe('API Routes', () => {
       expect(response.status).toBe(200);
 
       user = response.body;
-      console.log(user);
+ 
     });
-
     let gif = {
-      id: expect.any(Number),
       url: 'https://giphy.com/gifs/cat-smoke-smoking-3o6Zt481isNVuQI1l6',
       title: 'Cat Smoking GIF by sheepfilms',
       rating: 'pg-13',
@@ -52,6 +50,7 @@ describe('API Routes', () => {
 
       },
     };
+
     test('POST favorite', async () => {
       gif.userId = user.id;
       const response = await request
@@ -59,13 +58,53 @@ describe('API Routes', () => {
         .set('Authorization', user.token)
         .send(gif);
 
+     
       expect(response.status).toBe(200);
-      expect(response.body).toEqual(gif);
-      gif = response.body;
+      expect(response.body).toEqual({
+        id: expect.any(Number),
+        url: 'https://giphy.com/gifs/cat-smoke-smoking-3o6Zt481isNVuQI1l6',
+        title: 'Cat Smoking GIF by sheepfilms',
+        rating: 'pg-13',
+        images: {
+          'original': {
+            'height': '250',
+            'width': '400',
+            'size': '3662773',
+            'url': 'https://media1.giphy.com/media/3o6Zt481isNVuQI1l6/giphy.gif?cid=3f0609denlj9k1dmp3eudnl4h1pppz7svvmftuvikjldjdhf&rid=giphy.gif&ct=g',
+            'mp4_size': '772242',
+            'mp4': 'https://media1.giphy.com/media/3o6Zt481isNVuQI1l6/giphy.mp4?cid=3f0609denlj9k1dmp3eudnl4h1pppz7svvmftuvikjldjdhf&rid=giphy.mp4&ct=g',
+            'webp_size': '992406',
+            'webp': 'https://media1.giphy.com/media/3o6Zt481isNVuQI1l6/giphy.webp?cid=3f0609denlj9k1dmp3eudnl4h1pppz7svvmftuvikjldjdhf&rid=giphy.webp&ct=g',
+            'frames': '81',
+            'hash': '23c2bbc1d41e1376b8252ef4a6505bb2'
+          },
+        },
+        userId: 1
+      });
+      console.log(gif);
     }
     );
 
+    test('GET your favorites', async () => {
+      const response = await request
+        .get('/api/me/favorites')
+        .set('Authorization', user.token);
+    
+      expect(response.status).toBe(200);
+      expect(response.body).toEqual(gif);
+     
+    });
 
+    test('DELETE a favorite', async () => {
+      const response = await request
+        .delete(`/api/favorites/${gif.id}`)
+        .set('Authorization', user.token)
+        .send(gif);
+
+      expect(response.status).toBe(200);
+      expect(response.body).toBe(gif);
+
+    });
 
   });
 });
